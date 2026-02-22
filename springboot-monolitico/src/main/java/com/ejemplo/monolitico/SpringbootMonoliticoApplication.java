f1b3c2c12362cec9925d88993338cefa22fa72ce
Jessica <2021jessicaalvarez@gmail.com>
2026-02-22 16:12:32 -0600

    Add files via upload

---
+++ springboot-monolitico/src/main/java/com/ejemplo/monolitico/SpringbootMonoliticoApplication.java
@@ -0,0 +1,13 @@
+package com.ejemplo.monolitico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMonoliticoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootMonoliticoApplication.class, args);
+	}
+
+}
