5335fc6523694d71e32048d460be3f8cc7a1b6c6
Jessica <2021jessicaalvarez@gmail.com>
2026-02-22 15:54:16 -0600

    Add files via upload

---
+++ springboot-monolitico/src/test/java/com/ejemplo/monolitico/SpringbootMonoliticoApplicationTests.java
@@ -0,0 +1,14 @@
+package com.ejemplo.monolitico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootMonoliticoApplicationTests {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootMonoliticoApplication.class, args);
+	}
+
+}
